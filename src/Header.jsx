c5e6be8b0d81b84ed7cbea2402b9cfe232729e2c
Agnieszka Zachariasiewicz <agnieszka.kanarcia@gmail.com>
2026-02-26 00:55:37 +0100

    basic styling for header and got rid of all the root vite styles that were being annoying

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,12 +1,13 @@
 import { NavLink } from "react-router";
 import './styles/Header.css'
+import SettingsIcon from '@mui/icons-material/Settings';
 export default function Header(){
     return(
         <>
     <nav>
         <NavLink to="/">Diet planner app</NavLink>
         <NavLink to="/recipe-list">Recipes list</NavLink>
-        <NavLink to="/preferences">Preferences</NavLink>
+        <NavLink to="/preferences" className="preferences" aria-label="preferences"><SettingsIcon/></NavLink>
     </nav>
           </>
     )
