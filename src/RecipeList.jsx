250eb563b597e3b6b0dc3c1f419227fc0be320ae
Agnieszka Zachariasiewicz <agnieszka.kanarcia@gmail.com>
2026-04-28 13:57:18 +0200

    added no recipes found state handler in recipe list

--- src/RecipeList.jsx
+++ src/RecipeList.jsx
@@ -1,11 +1,20 @@
 import './styles/RecipeList.css'
 import RecipeCard from './RecipeCard.jsx'
+import AddNewRecipeButton from './AddNewRecipeButton.jsx'
 
 export default function RecipeList({recipesList, recipeActions, onItemClick, selectedIds}){
 
+    const noRecipesElement = (
+        <section className='empty-recipe-list'>
+            <p>No recipes found, try adding a new recipe.</p>
+            <AddNewRecipeButton/>
+        </section>
+    )
     return(
         <ul className='recipe-list'>
-            {recipesList.map(recipe=>{
+            {
+            recipesList.length === 0? noRecipesElement:    
+            recipesList.map(recipe=>{
                 return(
                     <RecipeCard 
                     key={recipe.id} 
